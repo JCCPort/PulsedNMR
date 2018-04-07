e37eead5f0077317b84a298dc2c275c957d8f514
Josh Porter <jccporter@gmail.com>
2018-04-07 16:37:43 +0100

    Improved converter.

--- converter.py
+++ converter.py
@@ -1,12 +1,17 @@
-from os import chdir, getcwd, listdir
+from tkinter import filedialog
 
 from pandas import read_csv
 
 
-def DataConvert(datafolder='DAT', destinationfolder='RDAT'):
-    chdir('C:\\Users\Josh\IdeaProjects\PulsedNMR\{}'.format(datafolder))
-    for filename in listdir(getcwd()):
-        name = filename.split('.')[0]
+def DataConvert():
+    datafolder = filedialog.askopenfilenames(initialdir="C:\\Users\Josh\IdeaProjects\PulsedNMR",
+                                             title="Select data to convert")
+    destinationfolder = filedialog.askdirectory(initialdir="C:\\Users\Josh\IdeaProjects\PulsedNMR",
+                                                title="Select folder to save converted data to")
+    for filename in datafolder:
+        print(filename)
+        name_ext = filename.split('/')[-1]
+        name = name_ext.split('.')[0]
         nam2 = name.split('_')
         nam3 = '{}_'.format(nam2[0])
         for i in range(1, len(nam2) - 1):
@@ -14,13 +19,7 @@ def DataConvert(datafolder='DAT', destinationfolder='RDAT'):
         nam4 = nam3 + ('R' + nam2[-1])
         if filename.split('.')[1] == 'csv':
             dframename = read_csv(filename, header=None, delimiter=',', usecols=[3, 4], engine='c')
-            chdir('C:\\Users\Josh\IdeaProjects\PulsedNMR\{}'.format(destinationfolder))
-            dframename.to_csv('{}.csv'.format(nam4), header=False, index=False)
         else:
             dframename = read_csv(filename, header=None, delim_whitespace=True, engine='c')
-            chdir('C:\\Users\Josh\IdeaProjects\PulsedNMR\{}'.format(destinationfolder))
-            dframename.to_csv('{}.csv'.format(nam4), header=False, index=False)
-        chdir('C:\\Users\Josh\IdeaProjects\PulsedNMR\{}'.format(datafolder))
-
-
-DataConvert()
+        print(destinationfolder)
+        dframename.to_csv('{}/{}.csv'.format(destinationfolder, nam4), header=False, index=False)
