e494272b8de322d1a5b5df962a776d2b6c246adc
Josh Porter <jccporter@gmail.com>
2018-04-09 21:22:56 +0100

    Simple fitting for envelope.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import filedialog, Tk
 
 import matplotlib.pyplot as plt
 import numpy as np
+from lmfit.models import GaussianModel, LinearModel
 from pandas import read_csv, read_hdf
 from scipy import fftpack, interpolate
 from scipy.optimize import curve_fit
@@ -21,16 +22,16 @@ def find_nearest(array, value):
     return array[idx]
 
 
-def pick_dat():
+def pick_dat(initdir='RDAT', title="Select file", cols=['t', 'm']):
     root = Tk()
-    root.filename = filedialog.askopenfilename(initialdir="C:\\Users\Josh\IdeaProjects\PulsedNMR\RDAT",
-                                               title="Select file")
+    root.filename = filedialog.askopenfilename(initialdir="C:\\Users\Josh\IdeaProjects\PulsedNMR\{}".format(initdir),
+                                               title=title)
     filename_parts = root.filename.split('/')[-1]
     if 'csv' in root.filename:
-        data = read_csv(root.filename, names=['t', 'm'], engine='c')
+        data = read_csv(root.filename, names=cols, engine='c')
         return data, filename_parts
     elif 'h5' in root.filename:
-        data = read_hdf(root.filename, 'table', names=['t', 'm'], engine='c')
+        data = read_hdf(root.filename, 'table', names=cols, engine='c')
         return data, filename_parts
     else:
         print('Unexpected file type. Choose either a .csv or .h5 file.')
@@ -107,6 +108,80 @@ def FID_Exponential_fit():
     plt.show()
 
 
+def range_to_list():
+    dat1, filename1 = pick_dat("RDAT_Test", "Select dataset to draw from")
+    dat2, filename2 = pick_dat("Ranges", "Select ranges to create lists from", ['Lower Bound', 'LowerIndex',
+                                                                                'Upper Bound', 'UpperIndex'])
+    print(dat1, dat2)
+    xrange = []
+    yrange = []
+    xranges = {}
+    yranges = {}
+    for o in range(0, len(dat2)):
+        print('Lower Index: ', dat2['LowerIndex'][o])
+        print('Upper Index: ', dat2['UpperIndex'][o])
+        xrange.append((dat1['t'][dat2['LowerIndex'][o]:dat2['UpperIndex'][o] + 1]).values)
+        yrange.append((dat1['m'][dat2['LowerIndex'][o]:dat2['UpperIndex'][o] + 1]).values)
+    for o in range(0, len(xrange)):
+        xranges[o] = xrange[o]
+        yranges[o] = yrange[o]
+    return xranges, yranges, xrange, yrange
+
+
+def echo_gaussian_fits():
+    xrs, yrs, xr, yr = range_to_list()
+    cents = []
+    cents_uncert = []
+    heights = []
+    heights_uncert = []
+    for i in range(0, len(xrs)):
+        mdl = GaussianModel(prefix='G_')
+        lne = LinearModel(prefix='L_')
+        params = mdl.guess(yrs[i], x=xrs[i])
+        params += lne.guess(yrs[i], x=xrs[i])
+        # minx = np.min(xrs[i])
+        # maxx = np.max(xrs[i])
+        # miny = np.min(yrs[i])
+        # maxy = np.max(yrs[i])
+        # params.add('G_center', value=np.mean(xrs[i]), min=minx*0.9, max=maxx*1.1)
+        # params.add('G_height', min=0, max=maxy*1.2)
+        # params.add('G_amplitude', min=0, max=100)
+        # params.add('L_intercept', min=-1000, max=1000)
+        # params.add('L_slope', min=-100, max=10000)
+        model = mdl + lne
+        result = model.fit(yrs[i], params, x=xrs[i], method='leastsq')
+        print(result.params, '\n')
+        plt.plot(xrs[i], result.best_fit)
+        plt.plot(xrs[i], yrs[i], 'x', ms=2)
+        cent = result.params['G_center'].value
+        amp = result.params['G_height'].value
+        # inter = result.params['L_intercept'].value
+        # grad = result.params['L_slope'].value
+        # height = amp + ((cent*grad) + inter)
+        cents.append(cent)
+        cents_uncert.append(result.params['G_center'].stderr)
+        heights.append(amp)
+        # partial_amp = 1
+        # partial_grad = cent
+        # partial_x = grad
+        # partial_inter = 1
+        # amp_term = partial_amp*result.params['G_height'].stderr
+        # grad_term = partial_grad*result.params['L_slope'].stderr
+        # x_term = partial_x*0.1
+        # inter_term = partial_inter*result.params['L_intercept'].stderr
+        # print(amp_term, grad_term, x_term, inter_term)
+        height_uncert = result.params['G_height'].stderr
+        heights_uncert.append(height_uncert)
+    print(len(heights_uncert))
+    print(len(cents))
+    popt, pcov = curve_fit(echo_as_T2, xdata=cents, ydata=heights, sigma=heights_uncert, maxfev=30000,
+                           method='lm')
+    plt.plot(cents, echo_as_T2(cents, *popt))
+    plt.plot(cents, heights, 'x', ms=4)
+    plt.errorbar(cents, heights, xerr=cents_uncert, yerr=heights_uncert, fmt='.')
+    plt.show()
+
+
 def interped_fourier_transformer():
     dat, filename = pick_dat()
     len2 = 2 * len(dat['m'])
@@ -165,4 +240,16 @@ def fourier_curvefit():
     halfln = int(length / 2)
 
 
-fourier_transformer()
+def pick_ranges():
+    dat, filename = pick_dat('Select file to pick ranges in')
+    fig, ax = plt.subplots()
+    plt.title('{} Fourier Transformed'.format(filename))
+    figure, = ax.plot(dat['t'], dat['m'])
+    Sel = RangeTool(dat['t'], dat['m'], figure, ax, filename)
+    fig_manager = plt.get_current_fig_manager()
+    fig_manager.window.showMaximized()
+    plt.show()
+
+
+# pick_ranges()
+echo_gaussian_fits()
